5b4a995191a75fe1554ec5de4b998ca288a21185
dollyp98 <114004693+dollyp98@users.noreply.github.com>
2022-11-18 11:44:50 +0100

    Initial Commit

---
+++ AppNC1!/AppNC1_App.swift
@@ -0,0 +1,17 @@
+//
+//  AppNC1_App.swift
+//  AppNC1!
+//
+//  Created by Dolores Pastore on 18/11/22.
+//
+
+import SwiftUI
+
+@main
+struct AppNC1_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
